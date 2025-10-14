f3b29afbf3fe9f8a8f976ee4512dc8b068c71a65
dariushghasemi <ghasemi.dariush@yahoo.com>
2025-10-14 18:26:30 +0200

    creating submit file#2

---
+++ submit.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#SBATCH --job-name test
+#SBATCH --output %j_susie.log
+#SBATCH --partition cpuq
+#SBATCH --cpus-per-task 1
+#SBATCH --mem 16G
+#SBATCH --time 30-00:00:00
+
+source ~/.bashrc
+module -s load singularity/3.8.5
+
+# set some singularity directories depending on frontend/computing node/vm
+case $(hostname) in
+  hnode*)
+    export SINGULARITY_TMPDIR=/tmp/
+    export SINGULARITY_BIND="/cm,/exchange,/processing_data,/project,/scratch,/center,/group,/facility,/ssu"
+    ;;
+  cnode*|gnode*)
+    export SINGULARITY_TMPDIR=$TMPDIR
+    export SINGULARITY_BIND="/cm,/exchange,/processing_data,/project,/localscratch,/scratch,/center,/group,/facility,/ssu"
+    ;;
+  lin-hds-*)
+    export SINGULARITY_TMPDIR=/tmp/
+    export SINGULARITY_BIND="/processing_data,/project,/center,/group,/facility,/ssu,/exchange"
+    ;;
+  *)
+    export SINGULARITY_TMPDIR=/var/tmp/
+    ;;
+esac
+
+# run the pipeline
+make run
