33792714af496f273f0b9f289d51768bf909728e
dariushghasemi <ghasemi.dariush@yahoo.com>
2026-05-20 23:58:04 +0200

    fix: combine reports given variables class #4

--- workflow/scripts/combine_cs_lists.R
+++ workflow/scripts/combine_cs_lists.R
@@ -55,8 +55,8 @@ sum_files_input <- res_files_input %>%
   str_replace(".cslist", ".cssum")
 
 # combine results for the input seqids
-res_combined <- map_dfr(res_files_input, ~ fread(.x, colClasses = c(rep("character", 8))))
-rep_combined <- map_dfr(rep_files_input, fread)
+res_combined <- map_dfr(res_files_input, ~ fread(.x, colClasses = "character"))
+rep_combined <- map_dfr(rep_files_input, ~ fread(.x, colClasses = "character"))
 sum_combined <- map_dfr(sum_files_input, fread)
 
 #--------------#
